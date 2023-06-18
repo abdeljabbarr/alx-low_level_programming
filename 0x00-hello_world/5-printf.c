8fc20d0fd42126db24bb1223db817591ebd0b24b
abdeljabbarr <elmaqdoudy.a.fst@uhp.ac.ma>
2023-06-18 15:22:23 -0700

    my first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print a message using print function
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
